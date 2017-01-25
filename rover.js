da37ed0652cf1e20ece4c65e22920c70f8eae7e2
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-01-25 10:02:33 -0500

    Git Desktop Test

--- rover.js
+++ rover.js
@@ -51,6 +51,7 @@ function hitRockBack(rover) {
       break;
   };
 }
+<<<<<<< Updated upstream
 
 function testForRocksForward(rover) {
   for (i = 0; i < myRock.length; i++) {
@@ -248,6 +249,215 @@ function instructions() {
   };
 }
 
+=======
+
+function testForRocksForward(rover) {
+  for (i = 0; i < myRock.length; i++) {
+    if ((myRock[i][0] === rover.position[0]) && (myRock[i][1] === rover.position[1])) {
+      document.getElementById("Direction").innerHTML = "There is a rock!!!!";
+      console.log("There is a Rock!!!");
+      windows.alert("You Hit a Rock!");
+      hitRockForward(rover);
+    }
+  }
+}
+
+function testForRocksBack(rover) {
+  for (i = 0; i < myRock.length; i++) {
+    if ((myRock[i][0] === rover.position[0]) && (myRock[i][1] === rover.position[1])) {
+      document.getElementById("Direction").innerHTML = "There is a rock!!!!";
+      console.log("There is a Rock!!!");
+      windows.alert("You Hit a Rock!");
+      hitRockBack(rover);
+    }
+  }
+}
+/*
+function drawRover(rover){
+  var x = rover.position[1];
+  var y = rover.position[0];
+  var xy = x", "y;
+  //document.getElementById(xy).innerHTML = "X";
+  console.log(x, y);
+}
+*/
+
+function goForward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++
+      testForRocksForward(rover);
+      drawRover(rover);
+      if (rover.position[0] > 10) {
+        rover.position[0] = 0
+      } else if (rover.position[0] < 0) {
+        rover.position[0] = 10
+      } else {
+        break;
+      }
+    case 'E':
+      rover.position[1]++
+      testForRocksForward(rover);
+      if (rover.position[1] > 10) {
+        rover.position[1] = 0
+      } else if (rover.position[1] < 0) {
+        rover.position[1] = 10
+      } else {
+        break;
+      }
+    case 'S':
+      rover.position[0]--
+      testForRocksForward(rover);
+        if (rover.position[0] > 10) {
+          rover.position[0] = 0
+        } else if (rover.position[0] < 0) {
+          rover.position[0] = 10
+        } else {
+          break;
+        }
+    case 'W':
+      rover.position[1]--
+      testForRocksForward(rover);
+      if (rover.position[1] > 10) {
+        rover.position[1] = 0
+      } else if (rover.position[1] < 0) {
+        rover.position[1] = 10
+      } else {
+        break;
+      }
+  };
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  console.log(rover.direction);
+  document.getElementById("loc").innerHTML = rover.position[0] + ", " + rover.position[1];
+  document.getElementById("Direction").innerHTML = rover.direction
+//  document.getElementByClassName("loc").innerHTML = (rover.position[0] + ", " + rover.position[1]);
+//  document.g("loc").innerHTML = "Hello World";
+
+}
+
+  function goBack(rover) {
+    switch(rover.direction) {
+      case 'N':
+        rover.position[0]--
+        testForRocksBack(rover);
+        if (rover.position[0] > 10) {
+          rover.position[0] = 0
+        } else if (rover.position[0] < 0) {
+          rover.position[0] = 10
+        } else {
+          break;
+        }
+      case 'E':
+        rover.position[1]--
+        testForRocksBack(rover);
+        if (rover.position[1] > 10) {
+          rover.position[1] = 0
+        } else if (rover.position[1] < 0) {
+          rover.position[1] = 10
+        } else {
+          break;
+        }
+      case 'S':
+        rover.position[0]++
+        testForRocksBack(rover);
+        if (rover.position[0] > 10) {
+          rover.position[0] = 0
+        } else if (rover.position[0] < 0) {
+          rover.position[0] = 10
+        } else {
+          break;
+        }
+      case 'W':
+        rover.position[1]++
+        testForRocksBack(rover);
+        if (rover.position[1] > 10) {
+          rover.position[1] = 0
+        } else if (rover.position[1] < 0) {
+          rover.position[1] = 10
+        } else {
+          break;
+        }
+    };
+    document.getElementById("loc").innerHTML = rover.position[0] + ", " + rover.position[1];
+    document.getElementById("Direction").innerHTML = rover.direction
+    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+    console.log(rover.direction);
+}
+
+    function goRight(rover) {
+      switch(rover.direction) {
+        case 'N':
+          rover.direction = 'E'
+          break;
+        case 'E':
+          rover.direction = 'S'
+          break;
+        case 'S':
+          rover.direction = 'W'
+          break;
+        case 'W':
+          rover.direction = 'N'
+          break;
+      };
+      console.log(rover.direction);
+      document.getElementById("loc").innerHTML = rover.position[0] + ", " + rover.position[1];
+      document.getElementById("Direction").innerHTML = rover.direction
+}
+
+      function goLeft(rover) {
+        switch(rover.direction) {
+          case 'N':
+            rover.direction = 'W'
+            break;
+          case 'E':
+            rover.direction = 'N'
+            break;
+          case 'S':
+            rover.direction = 'E'
+            break;
+          case 'W':
+            rover.direction = 'S'
+            break;
+        };
+        console.log(rover.direction);
+        document.getElementById("loc").innerHTML = rover.position[0] + ", " + rover.position[1];
+        document.getElementById("Direction").innerHTML = rover.direction
+      }
+
+
+function instructions() {
+  var instruct = prompt("Command (f, b, l, r)?")
+  switch(instruct){
+    case 'f':
+      goForward(myRover);
+      instructions();
+    case 'b':
+      goBack(myRover);
+      instructions();
+    case 'r':
+      goRight(myRover);
+      instructions();
+    case 'l':
+      goLeft(myRover);
+      instructions();
+      case 'w':
+        goForward(myRover);
+        instructions();
+      case 's':
+        goBack(myRover);
+        instructions();
+      case 'd':
+        goRight(myRover);
+        instructions();
+      case 'a':
+        goLeft(myRover);
+        instructions();
+    case 'q':
+      break;
+  };
+}
+
+>>>>>>> Stashed changes
 function clickButton(e) {
   switch(e.keyCode) {
     case 38:
